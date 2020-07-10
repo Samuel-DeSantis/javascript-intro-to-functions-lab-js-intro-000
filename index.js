278b7889608f1677b09d10c10565a355cefd7d38
Samuel DeSantis <samuel.i.desantis@gmail.com>
2020-07-10 05:02:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function shout(string) {return string.toUpperCase();}
 function logShout(string) {console.log(Shout(string));}
 function whisper(string) {return string.toLowerCase();}
-function logWhisper(string) {console.log(whispher(string));}
+function logWhisper(string) {console.log(whisper(string));}
 function sayHiToGrandma(string) {
   if (string === whisper(string)) {return "I can't hear you!";}
   else if (string === shout(string)) {return "YES INDEED!"}
